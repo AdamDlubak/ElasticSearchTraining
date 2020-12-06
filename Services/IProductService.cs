68ddc34857276d4decd21f290703c558071503a5
Adam Dlubak <adam.dlubak@gmail.com>
2020-12-06 23:26:05 +0100

    Prepared ElasticSearch Product Service

--- Services/IProductService.cs
+++ Services/IProductService.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using ElasticSearchTraining.Models;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace ElasticSearchTraining.Services
 {
-    public class IProductService
+    public interface IProductService
     {
+        Task<IEnumerable<Product>> GetProducts(int count, int skip = 0);
+        Task<Product> GetProductById(int id);
+        Task<IEnumerable<Product>> GetProductsByCategory(string category);
+        Task<IEnumerable<Product>> GetProductsByBrand(string category);
+        Task DeleteAsync(Product product);
+        Task SaveSingleAsync(Product product);
+        Task SaveManyAsync(Product[] products);
+        Task SaveBulkAsync(Product[] products);
     }
+}
 }
